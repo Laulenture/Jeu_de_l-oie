1fe97ac20705da2cc950931bd465f8e669ce6717
LAULENOS-PORTAB\Hikaru <l.aulen@ludus-academie.com>
2017-11-27 17:22:47 +0100

    no message

---
+++ jeu_de_loie.pas
@@ -0,0 +1,39 @@
+﻿program jeu_de_loie;
+
+uses crt;
+
+CONST
+    CASE_MORT = 58;
+    CASE_FIN = 66;
+    MULTIPLE = 9;
+
+VAR
+    pos,des : INTEGER;
+
+BEGIN
+    pos := 0;
+    
+    repeat
+        clrscr;
+        writeln('Vous êtes à la case ',pos);
+        {repeat
+            writeln('Entrez le résultat de vos dés');
+            readln(des);
+        until (des>=2) AND (des<=12);}
+        randomize;
+        des := random(11)+2;
+        pos := pos+des;
+        IF (pos MOD MULTIPLE = 0) AND (pos < CASE_FIN - MULTIPLE) AND (pos > 1) THEN
+            pos := pos + des;
+        IF pos > CASE_FIN THEN
+            pos := CASE_FIN - (pos - CASE_FIN);
+        IF pos = CASE_MORT THEN
+            BEGIN       
+                writeln('PAS DE CHANCE, RETOUR AU DEBUT');
+                pos := 0;
+            END;
+        readln();
+    until (pos = CASE_FIN);
+    writeln('Victoire');
+    readln();
+END.
